da90d1f63e3007086419b61a4ce9e8a22d9818fb
Rodrigo Bechara <rodribech20@gmail.com>
2018-10-05 16:10:31 -0300

    Allow remote url  to Bottom Bar.

--- ios/RCCTabBarController.m
+++ ios/RCCTabBarController.m
@@ -325,15 +325,12 @@
       id selectedIcon = actionParams[@"selectedIcon"];
       if (icon && icon != (id)[NSNull null])
       {
-        iconImage = [RCTConvert UIImage:icon];
-        //        iconImage = [[self image:iconImage withColor:self.tabBar.tintColor] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+        iconImage = [self convertIcon:icon];
         viewController.tabBarItem.image = [iconImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-        
       }
-      
       if (selectedIcon && selectedIcon != (id)[NSNull null])
       {
-        iconImageSelected = [[RCTConvert UIImage:selectedIcon] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+        iconImageSelected = [[self convertIcon:selectedIcon] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
         viewController.tabBarItem.selectedImage = iconImageSelected;
       } else {
         viewController.tabBarItem.selectedImage =  [[self image:iconImage withColor:self.tabBar.tintColor] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
@@ -366,6 +363,28 @@
   {
     completion();
   }
+}
+
+-(UIImage*) convertIcon:(id)icon {
+  if(icon[@"remoteUrl"] != nil) {
+    NSURL *url = [NSURL URLWithString:(NSString*)icon[@"uri"]];
+    if (url && url.scheme && url.host) {
+      NSData * imageData = [[NSData alloc] initWithContentsOfURL:url];
+      UIImage* tempImage = [UIImage imageWithData: imageData];
+      CGFloat width = 0;
+      CGFloat height = 0;
+      if(icon[@"width"] != nil) width = [icon[@"width"] floatValue];
+      if(icon[@"height"] != nil) height = [icon[@"height"] floatValue];
+      CGSize size = CGSizeMake(width, height);
+      UIGraphicsBeginImageContextWithOptions(size, NO, 0);
+      [tempImage drawInRect:CGRectMake(0, 0, size.width, size.height)];
+      UIImage *destImage = UIGraphicsGetImageFromCurrentImageContext();
+      UIGraphicsEndImageContext();
+      return destImage;
+    }
+  }
+  return [RCTConvert UIImage:icon];
+  
 }
 
 +(void)sendScreenTabChangedEvent:(UIViewController*)viewController body:(NSDictionary*)body{
