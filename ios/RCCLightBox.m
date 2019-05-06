10c70c77ade749d9dd6f0fb0307ee2d0e5803f7c
Rodrigo Bechara <rodribech20@gmail.com>
2019-05-06 16:49:34 -0300

    does not show on UIAlertController - Shows on  UIAlertController.presentingViewController

--- ios/RCCLightBox.m
+++ ios/RCCLightBox.m
@@ -222,6 +222,10 @@ const NSInteger kLightBoxTag = 0x101010;
     {
         return;
     }
+    
+    if([viewController isKindOfClass:[UIAlertController class]]) {
+        viewController = viewController.presentingViewController;
+    }
 
     RCCLightBoxView *lightBox = [[RCCLightBoxView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height) params:params];
     lightBox.tag = kLightBoxTag;
